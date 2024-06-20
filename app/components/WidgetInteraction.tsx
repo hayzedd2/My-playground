6b843c90ec35d0cf52a832aad7adcda5ca16bfee
hayzedd2 <azeezalhameen1@gmail.com>
2024-06-21 00:42:10 +0100

    added more widgets

--- app/components/WidgetInteraction.tsx
+++ app/components/WidgetInteraction.tsx
@@ -5,6 +5,7 @@ import { FaSpotify } from "react-icons/fa";
 import { FaAppStoreIos } from "react-icons/fa6";
 import { motion, useMotionValue } from "framer-motion";
 import { IoGiftOutline } from "react-icons/io5";
+import { MdOutlineModeEdit } from "react-icons/md";
 
 const WidgetInteraction = () => {
   const [currIndex, setCurrIndex] = useState(0);
@@ -13,45 +14,9 @@ const WidgetInteraction = () => {
 
   const widgets = [
     {
-      key: "widget-1",
-      content: (
-        <div className="widget bg-white widget-1">
-          <div className="flex justify-between">
-            <div className="song-title flex text-black gap-3 items-center">
-              <div className="w-12 h-12 rounded-[6px] songbox"></div>
-              <p className="font-[600] text-[0.85rem]">Forever Yours 3</p>
-            </div>
-            <div>
-              <FaSpotify />
-            </div>
-          </div>
-          <div className="song-content flex gap-3 items-center pt-3">
-            <div className="w-full h-20 rounded-[6px] songbox"></div>
-            <div className="w-full h-20 rounded-[6px] songbox"></div>
-            <div className="w-full h-20 rounded-[6px] songbox"></div>
-            <div className="w-full h-20 rounded-[6px] songbox"></div>
-          </div>
-        </div>
-      ),
-    },
-    {
-      key: "widget-2",
-      content: (
-        <div className=" widget widget-2 flex justify-between flex-col">
-          <div className="flex w-full justify-end">
-            <FaAppStoreIos className="text-black" />
-          </div>
-          <div>
-            <h3 className="font-[600] text-[0.75rem]">FEATURED</h3>
-            <p className="text-white">Our favourite online family games</p>
-          </div>
-        </div>
-      ),
-    },
-    {
       key: "widget-3",
       content: (
-        <div className="widget">
+        <div className="widget bg-white">
           <div className="flex gap-2">
             <div className="flex basis-[50%] flex-col">
               <div>
@@ -107,7 +72,9 @@ const WidgetInteraction = () => {
                   <div className="h-auto rounded-[8px] w-[4px] bg-[#1aadf8]"></div>
                   <div className="bg-[#E8F7FE] w-full rounded-[6px]">
                     <div className="flex flex-col ">
-                      <p className="font-[600] text-[#337fa6]">Bola&apos;s dinner</p>
+                      <p className="font-[600] text-[#337fa6]">
+                        Bola&apos;s dinner
+                      </p>
                       <p className="text-[#64b2db] font-[500] text-[0.85rem]">
                         8.00-10.00PM
                       </p>
@@ -122,6 +89,115 @@ const WidgetInteraction = () => {
                     3 more events
                   </p>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ),
+    },
+    {
+      key: "widget-1",
+      content: (
+        <div className="widget bg-white widget-1">
+          <div className="flex justify-between">
+            <div className="song-title flex text-black gap-3 items-center">
+              <div className="w-12 h-12 rounded-[6px] songbox"></div>
+              <p className="font-[600] text-[0.85rem]">Forever Yours 3</p>
+            </div>
+            <div>
+              <FaSpotify />
+            </div>
+          </div>
+          <div className="song-content flex gap-3 items-center pt-3">
+            <div className="w-full h-20 rounded-[6px] songbox"></div>
+            <div className="w-full h-20 rounded-[6px] songbox"></div>
+            <div className="w-full h-20 rounded-[6px] songbox"></div>
+            <div className="w-full h-20 rounded-[6px] songbox"></div>
+          </div>
+        </div>
+      ),
+    },
+    {
+      key: "widget-2",
+      content: (
+        <div className=" widget widget-2 flex justify-between flex-col">
+          <div className="flex w-full justify-end">
+            <FaAppStoreIos className="text-black" />
+          </div>
+          <div>
+            <h3 className="font-[600] text-[0.75rem]">FEATURED</h3>
+            <p className="text-white">Our favourite online family games</p>
+          </div>
+        </div>
+      ),
+    },
+    {
+      key: "widget-5",
+      content: (
+        <div className=" widget widget-4 flex items-end">
+          <div>
+            <h3 className="font-[600] text-[0.85rem]">ON THIS DAY</h3>
+            <p className="text-white text-[1.2rem]">June 20, 2024</p>
+          </div>
+        </div>
+      ),
+    },
+    {
+      key: "widget-2",
+      content: (
+        <div className=" widget widget-5 p-3 flex bg-[#282d31]">
+          <div className="basis-[30%] flex flex-col justify-between">
+            <div className="w-10 h-10 rounded-full flex items-center justify-center font-[500] bg-[#7E57C2] text-white">
+              <p>A</p>
+            </div>
+            <div className="w-10 h-10 flex items-center justify-center bg-[#414242] rounded-full">
+              <MdOutlineModeEdit className="text-[1.2rem] text-[#e0938f]" />
+            </div>
+          </div>
+          <div className="basis-[70%] flex flex-col gap-1">
+            <div className="flex bg-[#414242] p-1.5 rounded-[6px]">
+              <div className="flex flex-col justify-center gap-0 basis-[80%]">
+                <h6 className=" text-[#89898b] text-[0.75rem] font-[600]">
+                  Cowrywise
+                </h6>
+                <p className="text-[0.75rem] text-white">
+                  Your withdrawal has been successful
+                </p>
+              </div>
+              <div>
+                <p className="text-[0.75rem] font-[600] h-full flex items-center justify-center text-[#89898b] basis-[20%] ">
+                  9:40 PM
+                </p>
+              </div>
+            </div>
+            <div className="flex bg-[#414242] p-1.5 rounded-[6px]">
+              <div className="flex flex-col justify-center gap-0 basis-[80%]">
+                <h6 className=" text-[#89898b] text-[0.75rem] font-[600]">
+                  Vercel
+                </h6>
+                <p className="text-[0.75rem] text-white">
+                  Failed production environment
+                </p>
+              </div>
+              <div>
+                <p className="text-[0.75rem] font-[600] h-full flex items-center justify-center text-[#89898b] basis-[20%] ">
+                  8:16 PM
+                </p>
+              </div>
+            </div>
+            <div className="flex bg-[#414242] p-1 rounded-[6px]">
+              <div className="flex flex-col justify-center gap-0 basis-[80%]">
+                <h6 className=" text-[#89898b] text-[0.75rem] font-[600]">
+                  Glassdoor jobs
+                </h6>
+                <p className="text-[0.75rem] text-white">
+                  Lead software engineering jobs
+                </p>
+              </div>
+              <div>
+                <p className="text-[0.75rem] font-[600] h-full flex items-center justify-center text-[#89898b] basis-[20%] ">
+                  5:20 PM
+                </p>
               </div>
             </div>
           </div>
@@ -148,7 +224,13 @@ const WidgetInteraction = () => {
   return (
     <section className="flex-col min-h-screen xl:px-0 sm:px-4 py-5 max-w-[40rem] flex items-center justify-center mx-auto">
       <p className="my-2">
-        <input type="checkbox" name="" id="" checked={showOverflow} onClick={()=> setShowOverflow(!showOverflow)}/>
+        <input
+          type="checkbox"
+          name=""
+          id=""
+          defaultChecked={showOverflow}
+          onClick={() => setShowOverflow(!showOverflow)}
+        />
         Show overflow
       </p>
       <div className="flex gap-2">
@@ -163,7 +245,9 @@ const WidgetInteraction = () => {
           }}
           onDragStart={onDragStart}
           onDragEnd={onDragEnd}
-          className={`${showOverflow ? "" : "overflow-hidden"} widgetcontainer rounded-[16px] sm:w-[22rem] xl:w-[26rem] h-[11.5rem] cursor-grab indicator  flex flex-col`}
+          className={`${
+            showOverflow ? "" : "overflow-hidden"
+          } widgetcontainer rounded-[16px] sm:w-[22rem] xl:w-[26rem] h-[11.5rem] cursor-grab indicator  flex flex-col`}
         >
           {widgets.map((widget, index) => (
             <motion.div
@@ -214,7 +298,7 @@ const WidgetIndicator = ({
               currIndex == index
                 ? "h-[12px] rounded-[8px]"
                 : "h-[7px] rounded-full"
-            } w-[7px] bg-[#2c2c2c]`}
+            } w-[7px] bg-[#2c2c2c] cursor-pointer`}
             onClick={() => setCurrIndex(index)}
           ></div>
         ))}
